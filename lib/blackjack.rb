9b5e2bdf1e5659074d74f11ef19530f52ca22e0e
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 09:53:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,11 +41,11 @@ def hit?
   else
     invalid_command
   end
-  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
