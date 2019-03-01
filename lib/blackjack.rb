d6f7f310eebb7afb960ddc392def9392e7ff6f54
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 10:30:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,8 +56,11 @@ end
 
 def runner
   welcome
-  initial_round
-  hit?
-end_game
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
